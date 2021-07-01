fd61abcf3d83e803fe1b58be0910939ce3f45264
Vicas <vicasgasay@gmail.com>
2021-06-30 21:49:23 -0400

    Make the counter config driven, wow

---
+++ ini_defaults.py
@@ -0,0 +1,34 @@
+import configparser
+import os
+
+from pathlib import Path
+
+DIR_PATH = Path(os.path.dirname(os.path.realpath(__file__)))
+CONFIG_PATH = DIR_PATH / 'config.ini'
+
+def create_default_counter_config():
+    config = configparser.ConfigParser()
+    config['SETTINGS'] = {
+        'counter_name': 'Practice Counter',
+        'buttons': 'BUTTON1, BUTTON2',}
+    config['BUTTON1'] = {
+        'label': 'Success!',
+        'hotkey': 1,
+        'success': True,}
+    config['BUTTON2'] = {
+        'label': 'Failure!',
+        'hotkey': 2,
+        'success': False,}
+
+    with open(CONFIG_PATH, 'w') as configfile:
+        config.write(configfile)
+
+    return config
+
+# If there is a config file, load it. If a config file doesn't exist, write one
+def get_config_file_or_default():
+    if CONFIG_PATH.exists():
+        config = configparser.ConfigParser()
+        config.read(CONFIG_PATH)
+        return config
+    return create_default_counter_config()
